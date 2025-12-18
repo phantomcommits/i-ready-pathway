49c7aca786061b3a872ee01960d0c6f2a2ba3815
phantomcommits <steven203099@gmail.com>
2025-12-18 01:07:30 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,10 @@
     await import('./index.mjs');
   })();
 module.exports = app;
+// This replaces the old app.listen() part
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+
+module.exports = app;
